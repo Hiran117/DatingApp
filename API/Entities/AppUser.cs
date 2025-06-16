dfef1b96ddd031ac3db1d8371b2752f63dcb6c91
Hiran117 <handakahiran97@gmail.com>
2025-06-16 14:51:16 +0530

    pre integration

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,25 @@
+using System;
+using API.Extensions;
+using Microsoft.AspNetCore.Identity;
+
+namespace API.Entities;
+
+public class AppUser : IdentityUser<int>
+{
+public DateOnly DateOfBirth { get; set; }
+public required string KnownAs { get; set; }
+public DateTime Created { get; set; } = DateTime.UtcNow;
+public DateTime LastActive { get; set; } = DateTime.UtcNow;
+public required string Gender { get; set; }
+public string? Introduction { get; set; }
+public string? Intersts { get; set; }
+public string? LookingFor { get; set; }
+public required string City { get; set; }
+public required string Country { get; set; }
+public List<Photo> Photos { get; set; } =[];
+public List<UserLike> LikedByUsers { get; set; } = [];
+public List<UserLike> LikedUsers { get; set; } = [];
+public List<Message> MessagesSent { get; set; } = [];
+public List<Message> MessagesReceived { get; set; } = [];
+public ICollection<AppUserRole> UserRoles { get; set; } = [];
+}
