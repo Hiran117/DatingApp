a2fdff20f71670524b139b013e78250a01235afb
Hiran117 <handakahiran97@gmail.com>
2025-06-17 12:42:04 +0530

    Fix: Convert client from submodule to regular folder

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { NavComponent } from "./nav/nav.component";
+import { AccountService } from './_services/account.service';
+import { HomeComponent } from "./home/home.component";
+import { RouterOutlet } from '@angular/router';
+import { NgxSpinnerComponent } from 'ngx-spinner';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, NavComponent, NgxSpinnerComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit{
+
+  private accountService = inject(AccountService);
+  title = 'TestApp';
+  
+
+  ngOnInit(): void {
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
+  }
+
+}
---
+++ client/src/app/members/member-card/member-card.component.ts
@@ -0,0 +1,33 @@
+import { Component, computed, inject, input, ViewEncapsulation } from '@angular/core';
+import { Member } from '../../_models/member';
+import { RouterLink } from '@angular/router';
+import { LikesService } from '../../_services/likes.service';
+import { isDate } from 'ngx-bootstrap/chronos';
+import { PresenceService } from '../../_services/presence.service';
+
+@Component({
+  selector: 'app-member-card',
+  imports: [RouterLink],
+  templateUrl: './member-card.component.html',
+  styleUrl: './member-card.component.css',
+  encapsulation: ViewEncapsulation.None
+})
+export class MemberCardComponent {
+  private likeService = inject(LikesService);
+  private presenceService = inject(PresenceService);
+  member = input.required<Member>();
+  hasLiked = computed(() => this.likeService.likeIds().includes(this.member().id));
+  isOnline = computed(() => this.presenceService.onlineUsers().includes(this.member().username));
+
+  toggleLike() {
+    this.likeService.toggleLike(this.member().id).subscribe({
+      next: () => {
+        if (this.hasLiked()) {
+          this.likeService.likeIds.update(ids => ids.filter(x => x !== this.member().id))
+        } else {
+          this.likeService.likeIds.update(ids => [...ids, this.member().id])
+        }
+      }
+    })
+  }
+}
